5d218359d28c1a52c061ef2a425420d5d81ca403
SND\davedev_cp <SND\davedev_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2011-04-10 21:43:48 +0000

    New operator: DefaultIfEmpty.  (Will be included in the project in a subsequent changeset.)
    git-svn-id: https://rxx.svn.codeplex.com/svn@59476 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ Main/Source/Rxx/System/Linq/Observable2 - DefaultIfEmpty.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace System.Linq
+{
+	public static partial class Observable2
+	{
+		public static IObservable<TSource> DefaultIfEmpty<TSource>(
+			this IObservable<TSource> source)
+		{
+			Contract.Requires(source != null);
+			Contract.Ensures(Contract.Result<IObservable<TSource>>() != null);
+
+			return DefaultIfEmpty(source, default(TSource));
+		}
+
+		public static IObservable<TSource> DefaultIfEmpty<TSource>(
+			this IObservable<TSource> source,
+			TSource defaultValue)
+		{
+			Contract.Requires(source != null);
+			Contract.Ensures(Contract.Result<IObservable<TSource>>() != null);
+
+			var observable = Observable.CreateWithDisposable<TSource>(
+				observer =>
+				{
+					bool hasValue = false;
+
+					return source.Subscribe(
+						value =>
+						{
+							hasValue = true;
+							observer.OnNext(value);
+						},
+						observer.OnError,
+						() =>
+						{
+							if (!hasValue)
+								observer.OnNext(defaultValue);
+
+							observer.OnCompleted();
+						});
+				});
+
+			Contract.Assume(observable != null);
+
+			return observable;
+		}
+	}
+}
